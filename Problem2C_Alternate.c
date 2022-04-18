c1e28d6547c5ce70acbb223c108634b19e569aca
Ravi-Raghavan <56519163+Ravi-Raghavan@users.noreply.github.com>
2022-04-18 17:16:28 -0400

    Add files via upload

---
+++ Problem2C_Alternate.c
@@ -0,0 +1,90 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int SIZE;
+
+int * FIBONACCI;
+int done = 0;
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t c = PTHREAD_COND_INITIALIZER;
+void FibonacciIterative(int num){
+    for(int i = 0; i < num; i ++){
+        pthread_mutex_lock(&m);
+        
+        while(done == 1){
+            pthread_cond_wait(&c, &m);
+        }
+        
+        printf("Mutex Locked by FibonacciIterative \n");
+        done = 0;
+        if(i == 0){
+            *(FIBONACCI + i) = 0;
+            done = 1;
+            pthread_cond_signal(&c);
+            pthread_mutex_unlock(&m);
+            printf("Mutex Unlocked by FibonacciIterative \n");
+        }
+        else if(i == 1){
+            *(FIBONACCI + i) = 1;
+            done = 1;
+            pthread_cond_signal(&c);
+            pthread_mutex_unlock(&m);
+            printf("Mutex Unlocked by FibonacciIterative \n");
+        }
+        else{
+            *(FIBONACCI + i) = *(FIBONACCI + i - 1) + *(FIBONACCI + i - 2);
+            done = 1;
+            pthread_cond_signal(&c);
+            pthread_mutex_unlock(&m);
+            printf("Mutex Unlocked by FibonacciIterative \n");
+        }
+    }
+}
+
+void displayFibonacci(){
+    for(int i = 0; i< SIZE; i ++){
+        pthread_mutex_lock(&m);
+        while(done == 0){
+            pthread_cond_wait(&c, &m);
+        }
+        printf("Mutex Locked by displayFibonacci \n");
+        done = 1;
+        pthread_cond_signal(&c);
+        printf("The %d th Fibonacci Number is: %d\n", i, *(FIBONACCI + i));
+        pthread_mutex_unlock(&m);
+        printf("Mutex Unlocked by displayFibonacci \n");
+
+    }
+}
+
+
+int main(){
+    printf("Please Enter a Number Indicating the Number of Fibonacci Numbers You Would Like to Generate\n");
+    int x = scanf("%d", &SIZE);
+    FIBONACCI = (int*)(malloc(SIZE * sizeof(int)));
+
+    pthread_t threadID;
+    pthread_attr_t threadAttribute;
+
+    pthread_attr_init(&threadAttribute);
+    pthread_create(&threadID, &threadAttribute, FibonacciIterative, SIZE);
+    pthread_detach(&threadID);
+
+
+    displayFibonacci();
+
+
+}
+
+/**
+ * @brief 
+ * Basically, I had a global integer variable called done. Every time the child thread began a recursive execution of FibonacciRecursive(), 
+ * I created a lock for the child thread. When the child was done generating a fibonaci number and writing that number to array storage, 
+ * the child thread would signal to the parent thread that it was done using the condition variable. Furthermore, the child thread would release its lock 
+ * 
+ * 
+ */
---
+++ Problem2C_Backup.c
@@ -0,0 +1,74 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int SIZE;
+
+int * FIBONACCI;
+int count = 0;
+pthread_mutex_t mutexVariable = PTHREAD_MUTEX_INITIALIZER;
+void FibonacciIterative(int num){
+    for(; count < 2 * num;){
+        if(count % 2 == 0){
+            int index = count / 2;
+            pthread_mutex_lock(&mutexVariable);
+            printf("Mutex Locked by FibonacciIterative \n");
+            if(index == 0){
+                *(FIBONACCI + index) = 0;
+                pthread_mutex_unlock(&mutexVariable);
+                printf("Mutex Unlocked by FibonacciIterative \n");
+            }
+            else if(index == 1){
+                *(FIBONACCI + index) = 1;
+                pthread_mutex_unlock(&mutexVariable);
+                printf("Mutex Unlocked by FibonacciIterative \n");
+            }
+            else{
+                *(FIBONACCI + index) = *(FIBONACCI + index - 1) + *(FIBONACCI + index - 2);
+                pthread_mutex_unlock(&mutexVariable);
+                printf("Mutex Unlocked by FibonacciIterative \n");
+            }
+            count ++;
+        }
+    }
+}
+
+void displayFibonacci(){
+    for(; count < 2 * SIZE;){
+        if(count % 2 != 0){
+            int index = count / 2;
+            pthread_mutex_lock(&mutexVariable);
+            printf("Mutex Locked by displayFibonacci \n");
+            printf("The %d th Fibonacci Number is: %d\n", index, *(FIBONACCI + index));
+            count ++;
+            pthread_mutex_unlock(&mutexVariable);
+            printf("Mutex Unlocked by displayFibonacci \n");
+        }
+    }
+}
+
+int main(){
+    printf("Please Enter a Number Indicating the Number of Fibonacci Numbers You Would Like to Generate\n");
+    int x = scanf("%d", &SIZE);
+    FIBONACCI = (int*)(malloc(SIZE * sizeof(int)));
+
+    pthread_t threadID;
+    pthread_attr_t threadAttribute;
+
+    pthread_attr_init(&threadAttribute);
+    pthread_create(&threadID, &threadAttribute, FibonacciIterative, SIZE);
+    pthread_detach(&threadID);
+    displayFibonacci();
+}
+
+/**
+ * @brief 
+ * Basically, I had a global integer variable called done. Every time the child thread began a recursive execution of FibonacciRecursive(), 
+ * I created a lock for the child thread. When the child was done generating a fibonaci number and writing that number to array storage, 
+ * the child thread would signal to the parent thread that it was done using the condition variable. Furthermore, the child thread would release its lock 
+ * 
+ * 
+ */
